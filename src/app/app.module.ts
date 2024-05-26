f7e3439ce00ace75eba1e006fb76a9c870f8f3e7
mujeeb <mujeebrahmanps01707@gmail.com>
2024-05-26 16:25:07 +0530

    First commit,Project setup with modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { ChatComponent } from './features/chat/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
